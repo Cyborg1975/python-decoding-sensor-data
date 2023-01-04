76b167a86bc343820437b282c53bf34caa079653
Joel Perron <103043785+Cyborg1975@users.noreply.github.com>
2023-01-04 13:59:00 -0500

    Completed module

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -7,3 +7,8 @@ class HouseInfo:
 
     def get_data_by_area(self, field, rec_area=0):
         field_data = []
+    for record in self.data:
+        if rec_area == 0:
+            field_data.append(record[field])
+        elif rec_area == record['area']:
+            rec_area == int(record['area'])
