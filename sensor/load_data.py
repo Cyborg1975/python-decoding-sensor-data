6b7759b12a7bff460ea232d260f26293d25243c0
Joel Perron <103043785+Cyborg1975@users.noreply.github.com>
2023-01-04 13:23:08 -0500

    Completed module

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -5,6 +5,6 @@ def load_sensor_data():
     sensor_data = []
     sensor_files = glob.glob(os.path.join(os.getcwd(), "datasets", "*.csv"))
 
-    for sensor_file in sensor_files:
+    for sensor_files in sensor_file:
     #    with open ("sensor_file") as data_file:
     #        data_reader = csv.DictReader(data_file, delimiter=',')
