983e7965e21f3732b4fb7d187d6fabbbd8dedc1a
Joel Perron <103043785+Cyborg1975@users.noreply.github.com>
2023-01-05 13:35:24 -0500

    Completed module

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -25,6 +25,7 @@ print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
 test_date = datetime.strptime("5/9/20", "%m/%d/%y")
 recs = house_info.get_data_by_date("id", rec_date=test_date)
 print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
+
 # Module 3 code here:
 temperature_data = TemperatureData(data)
 recs = temperature_data.get_data_by_area(rec_area=test_area)
@@ -35,6 +36,7 @@ recs = temperature_data.get_data_by_date(rec_date=test_date)
 print("\nHouse Temperature sensor records for date: {} = {}".format(
     test_date.strftime("%m/%d/%y"), len(recs)))
 print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
+
 # Module 4 code here:
 humidity_data = HumidityData(data)
 recs = humidity_data.get_data_by_area(rec_area=test_area)
@@ -44,11 +46,12 @@ print("\tAverage: {} humidity".format(mean(recs)))
 recs = humidity_data.get_data_by_date(rec_date=test_date)
 print("\nHouse Humidity sensor records for area {} = {}".format(test_area, len(recs)))
 print("\tAverage: {} humidity".format(mean(recs)))
+
 # Module 5 code here:
+# Module 4B
 particle_data = ParticleData(data)
 recs = particle_data.get_data_by_area(rec_area=test_area)
-print("\nHouse Particle sensor records for area {} = {}".format(test_area, len(recs))
-
+print("\nHouse Particle sensor records for area {} = {}".format(test_area, len(recs)))
 concentrations = particle_data.get_data_concentrations(data=recs)
 print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
