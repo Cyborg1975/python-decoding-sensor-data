d120f5841652a2f8958a1ebb229d0f578630182a
Joel Perron <103043785+Cyborg1975@users.noreply.github.com>
2023-01-05 12:41:44 -0500

    Completed module

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -24,7 +24,7 @@ test_date = datetime.strptime("5/9/20", "%m/%d/%y")
 recs = house_info.get_data_by_date("id", rec_date=test_date)
 print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 # Module 4 code here:
-Temperature_data = TemperatureData(data)
+temperature_data = TemperatureData(data)
 recs = temperature_data.get_data_by_area(rec_area=test_area)
 print("\nHouse Temperature sensor records for area {} = {}".format(test_area, len(recs)))
 print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
