cdb6dc7c4c06a590f958568e6c048b5e4b258860
Joel Perron <103043785+Cyborg1975@users.noreply.github.com>
2023-01-05 13:08:45 -0500

    Completed module

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -4,7 +4,7 @@ from house_info import HouseInfo
 from datetime import date, datetime
 from temperature_info import TemperatureData
 from humidity_info import HumidityData
-
+from statistics import mean
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
