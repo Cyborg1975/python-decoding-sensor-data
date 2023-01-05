32af5f3f483b8ec516690e833e66a5e72e8de465
Joel Perron <103043785+Cyborg1975@users.noreply.github.com>
2023-01-05 01:05:40 -0500

    Completed module

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -1,11 +1,11 @@
-
+from the datetime import date, datetime
 
 
 class HouseInfo:
     def __init__(self, data):
         self.data = data
 
-    def get_data_by_area(self, field, rec_area=0):
+    def get_data_by_area(self, field, rec_area=date.today()):
         field_data = []
         for record in self.data:
             if rec_area == 0:
@@ -13,3 +13,6 @@ class HouseInfo:
             elif rec_area == int(record['area']):
                 field_data.append(record[field])
         return field_data
+
+    def get_data_by_date(self, field, rec_date):
+        self.field_data = []
